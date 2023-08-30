861a69a053ebd92a4e343c7fae4a530fd70dc8c6
k-kimpara <k-kimpara.16@nec.com>
2023-08-30 15:54:34 +0900

    pwa対応4

--- js/app.js
+++ js/app.js
@@ -18,7 +18,9 @@ function externalFunction() {
   localStorage.removeItem("ser");
   if (ser_local !== null) {
     tags.ser = ser_local;
-
+    document.getElementById("inputparam").value = ser_local;
+  } else {
+    document.getElementById("inputparam").value = "null";
   }
   OneSignal.User.addTags(tags);
   console.log('I am sending information');
