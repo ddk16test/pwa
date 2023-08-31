dcef82d7df58ce2e528bdfdd34d7a3d32dc0c32b
ddk16test <141709426+ddk16test@users.noreply.github.com>
2023-08-31 16:35:39 +0900

    pwa対応10

--- js/app.js
+++ js/app.js
@@ -1,5 +1,70 @@
-// var param = location.search;
-// document.getElementById("inputparam").value = param;
+const DB_NAME = "myDatabase";
+const OBJECT_STORE_NAME = "myObjectStore";
+
+function externalFunction() {
+  const tags = {
+    m: "kuroe"
+  };
+  //var ser_local = localStorage.getItem("ser");
+  //localStorage.removeItem("ser");
+  (async () => {
+    const ser_local = await getValue("ser");
+  })();
+  if (ser_local !== null) {
+    tags.ser = ser_local;
+    document.getElementById("inputparam_send").value = ser_local;
+  } else {
+    document.getElementById("inputparam_send").value = "null";
+  }
+  OneSignal.User.addTags(tags);
+  console.log('I am sending information');
+}
+
+function openDatabase() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, 1);
+
+    request.onsuccess = (event) => {
+      resolve(event.target.result);
+    }
+    request.onerror = (event) => {
+      reject(event.target.error);
+    };
+  });
+}
+
+function saveValue(key, value) {
+  return new Promise(async (resolve, reject) => {
+    const db = await openDatabase();
+    const transaction = db.transaction(OBJECT_STORE_NAME, "readwrite");
+    const objectStore = transaction.objectStore(OBJECT_STORE_NAME);
+
+    const request = objectStore.put(value, key);
+    request.onsuccess = (event) => {
+      resolve(event.target.result);
+    }
+    request.onerror = (event) => {
+      reject(event.target.error);
+    };
+  });
+}
+
+function getValue(key) {
+  return new Promise(async (resolve, reject) => {
+    const db = await openDatabase();
+    const transaction = db.transaction(OBJECT_STORE_NAME, "readwrite");
+    const objectStore = transaction.objectStore(OBJECT_STORE_NAME);
+
+    const request = objectStore.get(key);
+    request.onsuccess = (event) => {
+      resolve(event.target.result);
+    }
+    request.onerror = (event) => {
+      reject(event.target.error);
+    };
+  });
+}
+
 const url = window.location.href
 if (url !== null) {
   // for check
@@ -12,23 +77,10 @@ const ser = searchParams.get("ser");
 if (ser !== null) {
   // for check
   document.getElementById("inputparam").value = ser;
-  localStorage.setItem("ser", ser);
+  //localStorage.setItem("ser", ser);
+  (async () => {
+    await saveValue("ser", ser);
+  })();
 } else {
   document.getElementById("inputparam").value = "null";
-}
-
-function externalFunction() {
-  const tags = {
-    m: "kuroe"
-  };
-  var ser_local = localStorage.getItem("ser");
-  localStorage.removeItem("ser");
-  if (ser_local !== null) {
-    tags.ser = ser_local;
-    document.getElementById("inputparam_send").value = ser_local;
-  } else {
-    document.getElementById("inputparam_send").value = "null";
-  }
-  OneSignal.User.addTags(tags);
-  console.log('I am sending information');
 }
