5989959fa0ed6ebd34d1c79c867f84fb5882e3cd
ddk16test <141709426+ddk16test@users.noreply.github.com>
2023-09-08 16:54:40 +0900

    登録シーケンス終了トリガ1-4

--- js/webpush.js
+++ js/webpush.js
@@ -1,17 +1,19 @@
+const searchParams = new URLSearchParams(window.location.search);
+const ser_save = searchParams.get("ser"); 
+if (ser_save !== null) {
+    // for check
+    console.log('save localstrage');
+    localStorage.setItem("ser", ser_save);
+}
+
 window.OneSignalDeferred = window.OneSignalDeferred || [];
 OneSignalDeferred.push(function(OneSignal) {
-  // for debug
-  OneSignal.Debug.setLogLevel("trace");
-  OneSignal.init({
-    appId: "705dab8a-f3d1-4837-9932-708d34189bd5",
-  });
-  const searchParams = new URLSearchParams(window.location.search);
-    const ser_save = searchParams.get("ser"); 
-    if (ser_save !== null) {
-        // for check
-        localStorage.setItem("ser", ser_save);
-    }
-    OneSignal.Notifications.addEventListener("permissionPromptDisplay", subscribeEventListener);
+    // for debug
+    OneSignal.Debug.setLogLevel("trace");
+    OneSignal.init({
+        appId: "705dab8a-f3d1-4837-9932-708d34189bd5",
+    });
+    //OneSignal.Notifications.addEventListener("permissionPromptDisplay", subscribeEventListener);
 });
 
 // 通知許可イベントリスナー
