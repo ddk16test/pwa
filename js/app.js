3ae4641c5fb1695f33f1abdb9c6df0b02e98723c
ddk16test <141709426+ddk16test@users.noreply.github.com>
2023-09-08 15:19:45 +0900

    登録シーケンス終了トリガ1-2

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 function externalFunction() {
-  const tags = {
+  const tags_app = {
     m: "kuroe"
   };
   var ser_local = localStorage.getItem("ser");
   localStorage.removeItem("ser");
   if ((ser_local !== null) && (ser_local !== undefined)) {
-    tags.ser = ser_local;
+    tags_app.ser = ser_local;
     document.getElementById("inputparam_send").value = ser_local;
   } else {
     document.getElementById("inputparam_send").value = "null";
   }
-  OneSignal.User.addTags(tags);
+  OneSignal.User.addTags(tags_app);
   console.log('I am sending information');
 }
 
@@ -26,24 +26,9 @@ const ser = searchParams.get("ser");
 if (ser !== null) {
   // for check
   document.getElementById("inputparam").value = ser;
-  localStorage.setItem("ser", ser);
+  //localStorage.setItem("ser", ser);
 } else {
   document.getElementById("inputparam").value = "null";
 }
 
 
-// 通知許可イベントリスナー
-// 登録処理と同時にタグ送信するためsleep挟む必要あるか
-function permisssionChangeListener(permission) {
-  if (permission) {
-    console.log("permission accepted.");
-    externalFunction();
-  }
-}
-
-function subscribeEventListener(evnet) {
-  console.log("open subscribe prompt.");
-  OneSignal.Notifications.addEventListener("permissionChange", permissionChangeListener);
-}
-
-OneSignal.Notifications.addEventListener("permissionPromptDisplay", subscribeEventListener);
---
+++ js/webpush.js
@@ -0,0 +1,39 @@
+window.OneSignalDeferred = window.OneSignalDeferred || [];
+OneSignalDeferred.push(function(OneSignal) {
+  // for debug
+  OneSignal.Debug.setLogLevel("trace");
+  OneSignal.init({
+    appId: "705dab8a-f3d1-4837-9932-708d34189bd5",
+  });
+  const searchParams = new URLSearchParams(window.location.search);
+    const ser_save = searchParams.get("ser"); 
+    if (ser_save !== null) {
+        // for check
+        localStorage.setItem("ser", ser_save);
+    }
+});
+
+// 通知許可イベントリスナー
+// 登録処理と同時にタグ送信するためsleep挟む必要あるか
+function permisssionChangeListener(permission) {
+    if (permission) {
+        const tags = {
+            m: "kuroe"
+        };
+        console.log("permission accepted.");
+        var ser_send = localStorage.getItem("ser");
+        localStorage.removeItem("ser");
+        if ((ser_send !== null) && (ser_send !== undefined)) {
+            tags.ser = ser_send;
+        }
+        OneSignal.User.addTags(tags);
+        console.log('I am sending information');
+    }
+}
+  
+function subscribeEventListener(evnet) {
+    console.log("open subscribe prompt.");
+    OneSignal.Notifications.addEventListener("permissionChange", permissionChangeListener);
+}
+  
+OneSignal.Notifications.addEventListener("permissionPromptDisplay", subscribeEventListener);
