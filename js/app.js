4b1df7f68b2e33250221130a716892f87de3a3c6
ddk16test <141709426+ddk16test@users.noreply.github.com>
2023-09-26 15:34:45 +0900

    localStrage検証1

--- js/app.js
+++ js/app.js
@@ -5,12 +5,20 @@ if (url !== null) {
 } else {
   document.getElementById("url").value = "null";
 }
+
 const searchParams_app = new URLSearchParams(window.location.search);
 const ser = searchParams_app.get("ser"); 
 if (ser !== null) {
   // for check
-  document.getElementById("inputparam").value = ser;
-  //localStorage.setItem("ser", ser);
+  document.getElementById("inputparam_url").value = ser;
 } else {
-  document.getElementById("inputparam").value = "null";
+  document.getElementById("inputparam_url").value = "null";
+}
+
+var ser_strage;
+localStorage.setItem("ser", ser_strage);
+if (ser_strage !== null) {
+  document.getElementById("inputparam_strage").value = ser_strage;
+} else {
+  document.getElementById("inputparam_strage").value = "null";
 }
