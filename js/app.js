198675e12851e704f1c597e008176435cbde27be
ddk16test <141709426+ddk16test@users.noreply.github.com>
2023-10-04 16:01:24 +0900

    ボタンインストール_pwa4

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 const url = window.location.href;
 if (url !== null) {
   // for check
-  document.getElementById("url").value = url;
+  //document.getElementById("url").value = url;
 } else {
-  document.getElementById("url").value = "null";
+  //document.getElementById("url").value = "null";
 }
 
 const searchParams_app = new URLSearchParams(window.location.search);
 const ser = searchParams_app.get("ser"); 
 if (ser !== null) {
   // for check
-  document.getElementById("inputparam_url").value = ser;
+  //document.getElementById("inputparam_url").value = ser;
 } else {
-  document.getElementById("inputparam_url").value = "null";
+  //document.getElementById("inputparam_url").value = "null";
 }
 
 var ser_strage;
 ser_strage = localStorage.getItem("ser");
 if (ser_strage !== null) {
-  document.getElementById("inputparam_strage").value = ser_strage;
+  //document.getElementById("inputparam_strage").value = ser_strage;
 } else {
-  document.getElementById("inputparam_strage").value = "null";
+  //document.getElementById("inputparam_strage").value = "null";
 }
 
 
